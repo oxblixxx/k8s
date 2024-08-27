099a6b5f504169bae6858d0c8ae8c681402bbbb9
oXbLiXxX <97739416+oxblixxx@users.noreply.github.com>
2024-08-27 05:48:20 +0200

    Update initial_script.sh

--- initial_script.sh
+++ initial_script.sh
@@ -2,3 +2,8 @@
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install pip
 pip install ansible
+mkdir ~/k8s-osh
+cd ~/k8s-osh
+git clone https://opendev.org/openstack/openstack-helm-infra.git
+git clone https://opendev.org/zuul/zuul-jobs.git
+export ANSIBLE_ROLES_PATH=~/k8s-osh/openstack-helm-infra/roles:~/k8s-osh/zuul-jobs/roles
