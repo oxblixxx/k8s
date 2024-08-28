f108c776a90fad731fa78beddf93aa3ff46f7278
oxblixxx <phamust1111@gmail.com>
2024-08-28 20:14:28 +0000

    added scripts && inventory

--- initial_script.sh
+++ initial_script.sh
@@ -1,9 +1,10 @@
+
 #!/bin/bash
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install pip
-pip install ansible
+#sudo apt install pip -y
+sudo apt install ansible -y
 mkdir ~/k8s-osh
 cd ~/k8s-osh
 #git clone https://opendev.org/openstack/openstack-helm-infra.git
 #git clone https://opendev.org/zuul/zuul-jobs.git
-export ANSIBLE_ROLES_PATH=~/k8s-osh/openstack-helm-infra/roles:~/k8s-osh/zuul-jobs/roles
+export ANSIBLE_ROLES_PATH=~/k8s/openstack-helm-infra/roles:~/k8s/zuul-jobs/roles
